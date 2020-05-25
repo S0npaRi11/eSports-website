01b713cc401d6102a631f69f859596848075492e
Parag Mahale <54019264+S0npaRi11@users.noreply.github.com>
2020-05-25 13:34:45 +0000

    Add files via upload

--- routes/register.js
+++ routes/register.js
@@ -3,9 +3,25 @@ if(process.env.NODE_ENV !== 'production'){
 }
 
 const express = require('express');
-const participants = require('../models/Participant');;
+const participants = require('../models/Participant');
+const email = require('./emailConfig');
 
 const router = express.Router();
+
+const nodeMailer = require('nodemailer');
+
+const transporter = nodeMailer.createTransport({
+    service: 'gmail',
+    auth : {
+        type: 'Oauth2',
+        user: process.env.EMAIL_SEND,  // import from .env
+        pass: process.env.EMAIL_PASS,  // import from .env
+        clientId: process.env.EMAIL_SENDER_CLIENT_ID,  //import from .env
+        clientSecret: process.env.EMAIL_SENDER_CLIENT_SECRET,  // import from .env
+        refreshToken: process.env.EMAIL_SENDER_REFERSH_TOKEN,  // import from .env
+        accessToken: process.env.EMAIL_SENDER_ACCESS_TOKEN  // import from .env
+    }
+})
 
 router.post('/', (req,res) =>{
     req.session.participant = req.body;
@@ -29,6 +45,61 @@ router.get('/success', (req,res)=> {
     });
 
     newParticipant.save().then(() => {
+        // create the temporary array of the emails
+        const emails = [];
+
+        // if not null, push the email in the emails array
+        if(req.session.participant.player1Email != ""){
+            emails.push(req.session.participant.player1Email);
+        }
+        if(req.session.participant.player2Email != ""){
+            emails.push(req.session.participant.player2Email);
+        }
+        if(req.session.participant.player3Email != ""){
+            emails.push(req.session.participant.player3Email);
+        }
+        if(req.session.participant.player4Email != ""){
+            emails.push(req.session.participant.player4Email);
+        }
+       
+
+        console.log(emails);
+        // build the template here
+        let template = `
+        <h2>  You have been successfully registered for </h2>
+        <h1> Monthly Tournament PUBG JUNE 2020 </h1>
+        <b> Details: </b>
+        <ul>  
+            <li>Squad Name: ${req.session.participant.teamName}  </li>
+            <li>Player 1 Name: ${req.session.participant.player1}  </li>
+            <li>Player 2 Name: ${req.session.participant.player2}  </li>
+            <li>Player 3 Name: ${req.session.participant.player3}  </li>
+            <li>Player 4 Name: ${req.session.participant.player4}  </li>
+            <li>  <hr />  </li>
+            <li> Payment: Successfull </li>
+        </ul>
+        <p> 
+            Please read the rules for the tournament. <a href="https://www.monthly-tournament.herokuapp.com/rules"> Click here </a> to read the rules. <br />
+            <b>  All the further details reguarding the tournamet will be provided via email </b>
+        </p>
+        `;
+
+        // sending email for every player of the team
+        emails.forEach( em => {
+            const mailOptions = {
+                from: process.env.EMAIL_SEND,  // sender's email
+                to: em, // receiver's email
+                subject: 'Purchase Receipt',
+                html:   template// template gose here
+            }
+            // send the registration email form here
+            transporter.sendMail(mailOptions, err => {
+                if(err) console.log(err);
+                else{
+                    console.log('email sent to ' + em);
+                }
+            });
+        })
         res.render('../views/success.ejs');
     }).catch(() => {
         res.render('../views/500.ejs');
